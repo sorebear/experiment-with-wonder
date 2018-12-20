b998f47dd5289c87dce585e5f67170f5f9db784d
Soren Baird <sorenbaird@gmail.com>
2018-12-19 16:35:17 -0800

    Added Amy Maxwell's recommendation and updated bio to link to Gap Community

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// import amyMaxwell from '../assets/images/amy-maxwell.png';
+import amyMaxwell from '../assets/images/amy-maxwell.png';
 import jeanMarieJobs from '../assets/images/jean-marie-jobs.png';
 import johnOrtberg from '../assets/images/john-ortberg.png';
 import steveCarter from '../assets/images/steve-carter.png';
@@ -13,7 +13,7 @@ const Testimonials = () => (
           <h2>What Others Are Saying</h2>
         </header>
       </div>
-      <div className="col-4">
+      <div className="col-3">
         <span className="image fit max-width-350"><img src={johnOrtberg} alt="John Ortberg" /></span>
         <h3 style={{ marginBottom: 0 }}>John Ortberg</h3>
         <p style={{ opacity: .65 }}>
@@ -21,19 +21,19 @@ const Testimonials = () => (
             Author of All the Places to Go, Soul Keeping, Who is this Man?, and many&nbsp;more.
           </em>
         </p>
-        <p>&quot;Caitlin has a wonderful gift for being able to communicate in a way that changes lives.  She is thoughtful, honest, warm, reflective, and deeply challenging.&quot;</p>
+        <p>&quot;Caitlin has a wonderful gift for being able to communicate in a way that changes lives. She is thoughtful, honest, warm, reflective, and deeply challenging.&quot;</p>
       </div>
-      <div className="col-4">
-        <span className="image fit max-width-350"><img src={jeanMarieJobs} alt="Jean Marie Jobs" /></span>
-        <h3 style={{ marginBottom: 0 }}>Jean Marie Jobs</h3>
+      <div className="col-3">
+        <span className="image fit"><img src={amyMaxwell} alt="Amy Maxwell" /></span>
+        <h3 style={{ marginBottom: 0 }}>Amy Maxwell</h3>
         <p style={{ opacity: .65 }}>
           <em>
-            Master trainer, Founder of GAP&nbsp;Community and author of The Art of Feeding&nbsp;Heroes.
+            Master trainer and coach, Director of International Projects, GAP&nbsp;Community
           </em>
         </p>
-        <p>&quot;Caitlin’s coaching and training style is both creative and insightful. She continually challenges her self to go beyond perceived limitations and is courageous in her stand for others. She is an absolute delight!&quot;</p>
+        <p>&quot;Caitlin&apos;s coaching makes a powerful impression on every person she works with. Her ability to help you question your own limitations will leave you with a renewed sense of freedom and purpose.  She is committed focused and genuine.  If you are looking to accelerate your mission in life, whatever it may be, I highly recommend hiring Caitlin.&quot;</p>
       </div>
-      <div className="col-4">
+      <div className="col-3">
         <span className="image fit max-width-350"><img src={steveCarter} alt="Steve Carter" /></span>
         <h3 style={{ marginBottom: 0 }}>Steve Carter</h3>
         <p style={{ opacity: .65 }}>
@@ -43,16 +43,16 @@ const Testimonials = () => (
         </p>
         <p>&quot;Caitlin is a dear friend who has taught me so much about grace. She has a way with words, is brave beyond measure and simply reminds me of the love of Christ by the way she chooses to orient her life. She will make you laugh, make you think and consistently ask you the right questions.&quot;</p>
       </div>
-      {/* <div className="col-3">
-        <span className="image fit"><img src={amyMaxwell} alt="Amy Maxwell" /></span>
-        <h3 style={{ marginBottom: 0 }}>Amy Maxwell</h3>
+      <div className="col-3">
+        <span className="image fit max-width-350"><img src={jeanMarieJobs} alt="Jean Marie Jobs" /></span>
+        <h3 style={{ marginBottom: 0 }}>Jean Marie Jobs</h3>
         <p style={{ opacity: .65 }}>
           <em>
-            Master trainer and coach, Director of International Projects, GAP&nbsp;Community
+            Master trainer, Founder of GAP&nbsp;Community and author of The Art of Feeding&nbsp;Heroes.
           </em>
         </p>
-        <p>""</p>
-      </div> */}
+        <p>&quot;Caitlin’s coaching and training style is both creative and insightful. She continually challenges her self to go beyond perceived limitations and is courageous in her stand for others. She is an absolute delight!&quot;</p>
+      </div>
     </div>
   </section>
 );
